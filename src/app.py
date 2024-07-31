0714de613611f04ca8fa6665596578ae32038a30
Jonathan Huster <not_a_real_name@fake_domain.com>
2024-07-30 22:17:32 -0600

    Make initial endpoints with descriptions

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@
 import pandas as pd
 from flask import Flask, jsonify
 from memory_db import DataFrameDb
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 def set_up()->DataFrameDb:
-    df = pd.read_csv(r"C:\Users\jonat\Downloads\Root Beer Ratings - Sheet1.csv")
+    df = pd.read_csv("data/root_beer.csv")
     in_mem = DataFrameDb()
     in_mem.import_data(df)
     return in_mem
@@ -18,12 +18,18 @@ def hello():
     return "Hello World!"
 
 @app.route("/root_beer/name_in/<name>")
+@swag_from("root_beer/name_in.yml")
 def name_in(name):
-    """
-    file: root_beer_specs.yml
-    """
     df_db = set_up()
     return str(df_db.name_present(name))
+
+@app.route("/root_beer/names")
+@swag_from("root_beer/names.yml")
+def all_names():
+    df_db = set_up()
+    names = list(df_db.data["Name"])
+    print(names)
+    return jsonify({"hello": names})
 
 if __name__ == "__main__":
     app.run(debug=True)
